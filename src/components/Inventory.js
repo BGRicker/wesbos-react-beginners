38f2f7733d70eb42fbb1baf25fd7da986c63e4dd
Ben Ricker <bgricker@gmail.com>
2018-10-30 22:52:52 -0400

    13 - understanding state

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,12 +1,15 @@
 import React from "react";
+import AddFishForm from './AddFishForm';
 
 class Inventory extends React.Component {
     render() {
         return (
             <div className="inventory">
-                Inventory!
+                <h2>Inventory</h2>
+                <AddFishForm addFish={this.props.addFish} />
+                {/* this doesn't live in inventory, it's just been passed in via props */}
             </div>
-        )
+        );
     }
 }
 
